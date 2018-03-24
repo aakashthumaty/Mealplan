858b8c6322ed7cd2a2ede2892f8fe88722652258
Aakash Thumaty <aakashthumaty@gmail.com>
2018-03-24 16:50:10 -0400

    Initial Commit

---
+++ Mealplan/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mealplan
+//
+//  Created by Aakash Thumaty on 3/24/18.
+//  Copyright © 2018 Aakash Thumaty. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
